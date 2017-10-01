503e077c7a5add4d39970e129ba0cbbd2e5053a1
Andrew Timney <andrew.timney@gmail.com>
2017-10-01 22:30:50 +0100

    linter

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Card, CardBlock, CardTitle, CardSubtitle, Button } from 'reactstrap';
-import { Link } from 'react-router-dom';
 
 const Order = props => (
     <div>
